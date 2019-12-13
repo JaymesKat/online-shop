2c3c7c3da538c80ecb601f1a531b1244febe5c2d
James Katarikawe <james.katarikawe@andela.com>
2019-12-13 12:52:25 +0300

    Add mongodb database setup

---
+++ util/database.js
@@ -0,0 +1,25 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://root:cRpZUMjEnWKC0dcB@cluster0-aylow.mongodb.net/shop?retryWrites=true&w=majority', { useUnifiedTopology: true})
+    .then(client => {
+        _db = client.db();
+        callback();
+    }).catch(err => {
+        console.log(err);
+        throw err;
+    });
+}
+
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+module.exports.mongoConnect = mongoConnect;
+module.exports.getDb = getDb;
